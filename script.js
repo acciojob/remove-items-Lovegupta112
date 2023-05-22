e8b30483a818d933a747161c76ce8a14b3197d56
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-22 09:21:32 +0530

    add remove color functionality

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+let colorSelect = document.getElementById('colorSelect');
+// console.log(colorSelect);
+
+function removeColor() {
+    let optionValue = colorSelect.value;
+    // console.log(optionValue);
+    let options = document.getElementsByTagName('option');
+    // console.log(options[3].value);
+
+    for (let option of options) {
+        if (option.value === optionValue) {
+            // console.log(option.value);
+            option.remove();
+        }
+    }
+}
